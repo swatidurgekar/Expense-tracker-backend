1b1146a4325c254aba71e54f93cf058678f1ba4b
swatidurgekar <swatidurgekar@gmail.com>
2023-09-10 19:53:47 +0530

    Creating Login Screen Frontend

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/user");
 
 router.get("/", userController.signUpForm);
+router.get("/login", userController.loginForm);
 router.post("/user/signUp", userController.postSignUpForm);
+router.post("/user/login", userController.postLoginForm);
 
 module.exports = router;
--- controllers/user.js
+++ controllers/user.js
@@ -4,18 +4,64 @@ exports.signUpForm = (req, res, next) => {
   res.render("signUpForm.html");
 };
 
+exports.loginForm = (req, res, next) => {
+  res.render("loginForm.html");
+};
+
 exports.postSignUpForm = (req, res, next) => {
   const { name, username, password } = req.body;
+  const stringValidation = (string) => {
+    if (!string) return true;
+  };
+  if (
+    stringValidation(name) ||
+    stringValidation(username) ||
+    stringValidation(password)
+  )
+    return res.status(500).json({ message: "please enter all details" });
   User.create({
     name,
     username,
     password,
   })
     .then(() => {
-      res.redirect("/");
+      res.redirect("/login");
     })
     .catch((err) => {
       res.status(500).json({ message: "user exists" });
       console.log(err.errors.message);
+    });
+};
+
+exports.postLoginForm = (req, res, next) => {
+  const { username, password } = req.body;
+  let foundUser;
+  const stringValidation = (string) => {
+    if (!string) return true;
+  };
+  if (stringValidation(username) || stringValidation(password)) {
+    return res.status(500).json({ message: "please enter all details" });
+  }
+  User.findAll()
+    .then((users) => {
+      for (let user of users) {
+        if (user.username === username) {
+          foundUser = user;
+          return user;
+        }
+      }
+      return res.status(500).json({ message: "user not found" });
+    })
+    .then((user) => {
+      if (foundUser) {
+        if (user.password === password) {
+          res.redirect("/login");
+        } else {
+          res.status(500).json({ message: "incorrect password" });
+        }
+      }
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
