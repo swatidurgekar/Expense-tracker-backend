e7e3b5b8253830247f3e93046afcda0b026b897c
swatidurgekar <swatidurgekar@gmail.com>
2023-09-11 00:11:06 +0530

    Protecting user's password

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
+const { JSON } = require("sequelize");
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 exports.signUpForm = (req, res, next) => {
   res.render("signUpForm.html");
@@ -9,60 +11,72 @@ exports.loginForm = (req, res, next) => {
 };
 
 exports.postSignUpForm = (req, res, next) => {
-  const { name, username, password } = req.body;
-  const stringValidation = (string) => {
-    if (!string) return true;
-  };
-  if (
-    stringValidation(name) ||
-    stringValidation(username) ||
-    stringValidation(password)
-  )
-    return res.status(500).json({ message: "please enter all details" });
-  User.create({
-    name,
-    username,
-    password,
-  })
-    .then(() => {
-      res.redirect("/login");
-    })
-    .catch((err) => {
-      res.status(500).json({ message: "user exists" });
-      console.log(err.errors.message);
+  try {
+    const { name, username, password } = req.body;
+    const stringValidation = (string) => {
+      if (!string) return true;
+    };
+    if (
+      stringValidation(name) ||
+      stringValidation(username) ||
+      stringValidation(password)
+    ) {
+      return res.status(500).json({ message: "please enter all details" });
+    }
+
+    bcrypt.hash(password, 10, async (err, hashValue) => {
+      if (err) {
+        throw new Error();
+      } else {
+        const creation = User.create({
+          name,
+          username,
+          password: hashValue,
+        })
+          .then(() => {
+            res.redirect("/");
+          })
+
+          .catch((err) => {
+            return res.status(500).json({ message: "user exists" });
+          });
+      }
     });
+  } catch (err) {
+    return res.status(500).json({ message: "user exists" });
+  }
 };
 
-exports.postLoginForm = (req, res, next) => {
-  const { username, password } = req.body;
-  let foundUser;
-  const stringValidation = (string) => {
-    if (!string) return true;
-  };
-  if (stringValidation(username) || stringValidation(password)) {
-    return res.status(500).json({ message: "please enter all details" });
+exports.postLoginForm = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const stringValidation = (string) => {
+      if (!string) return true;
+    };
+    if (stringValidation(username) || stringValidation(password)) {
+      return res.status(500).json({ message: "please fill all fields!" });
+    }
+    const user = await User.findAll({ where: { username } });
+    console.log("user", user);
+    if (user.length > 0) {
+      if (user[0].username === username) {
+        bcrypt.compare(password, user[0].password, (err, result) => {
+          if (err)
+            return res.status(500).json({ message: "something went wrong" });
+          else if (result) {
+            return res
+              .status(200)
+              .json({ success: true, message: "user logged in successfully." });
+          } else {
+            console.log("incorrect password");
+            return res.status(500).json({ message: "incorrect password" });
+          }
+        });
+      }
+    } else {
+      return res.status(500).json({ message: "user not found!" });
+    }
+  } catch (err) {
+    console.log(err.message);
   }
-  User.findAll()
-    .then((users) => {
-      for (let user of users) {
-        if (user.username === username) {
-          foundUser = user;
-          return user;
-        }
-      }
-      return res.status(500).json({ message: "user not found" });
-    })
-    .then((user) => {
-      if (foundUser) {
-        if (user.password === password) {
-          //   res.json({ message: "user login successfull" });
-          res.redirect("/login");
-        } else {
-          res.status(500).json({ message: "incorrect password" });
-        }
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 };
